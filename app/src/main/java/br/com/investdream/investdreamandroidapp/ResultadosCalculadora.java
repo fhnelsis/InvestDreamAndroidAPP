9739ef3c07d4d2565659da24037407c93d15c839
Francisco Nelsis <fhnelsis@outlook.com>
2015-07-03 00:11:46 -0300

    20150703 - Correção de parseDouble no cálculo.

--- app/src/main/java/br/com/investdream/investdreamandroidapp/ResultadosCalculadora.java
+++ app/src/main/java/br/com/investdream/investdreamandroidapp/ResultadosCalculadora.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -21,42 +23,50 @@ public class ResultadosCalculadora extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultados_calculadora);
 
+        Button novoCalculo = (Button) findViewById(R.id.btNovoCalculo);
+
+        novoCalculo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                novoCalculo();
+            }
+        });
+
         try {
             Bundle bundle = getIntent().getExtras();
 
-            Double valorDoBem = bundle.getDouble("valorEntragaPaga");
-            Double valorEntradaPaga = bundle.getDouble("valorDoBem");
+//            Double valorDoBem = Double.parseDouble(bundle.getString(valorDoBem));
+            Double valorEntradaPaga = Double.parseDouble(bundle.getString("valorEntragaPaga"));
 
-            Double upgrade = (valorDoBem * 1.25);
-            Double valorDoTitulo = (upgrade * 2);
-            Double valorDaParcela = (upgrade / 60);
-            Double saldoAPagar = (valorDoTitulo - valorEntradaPaga);
-            Double valorDaParcelaFinal = (saldoAPagar / 90);
+//            Double upgrade = valorDoBem * 1.25;
+//            Double valorDoTitulo = (upgrade * 2);
+//            Double valorDaParcela = (upgrade / 60);
+//            Double saldoAPagar = (valorDoTitulo - valorEntradaPaga);
+//            Double valorDaParcelaFinal = (saldoAPagar / 90);
 
             String StringValorEntradaPaga = valorEntradaPaga.toString();
-            String StringUpgrade = upgrade.toString();
-            String StringValorDoTitulo = valorDoTitulo.toString();
-            String StringValorDaParcela = valorDaParcela.toString();
-            String StringSaldoAPagar = saldoAPagar.toString();
-            String StringValorDaParcelaFinal = valorDaParcelaFinal.toString();
+//            String StringUpgrade = upgrade.toString();
+//            String StringValorDoTitulo = valorDoTitulo.toString();
+//            String StringValorDaParcela = valorDaParcela.toString();
+//            String StringSaldoAPagar = saldoAPagar.toString();
+//            String StringValorDaParcelaFinal = valorDaParcelaFinal.toString();
 
             TextView txtViewValorEntradaPaga = (TextView) findViewById(R.id.resultadosCalculadoraValueValorEntradaPaga);
             txtViewValorEntradaPaga.setText(StringValorEntradaPaga);
 
-            TextView txtViewUpgrade = (TextView) findViewById(R.id.resultadosCalculadoraValueUp);
-            txtViewUpgrade.setText(StringUpgrade);
-
-            TextView txtViewValorDoTitulo = (TextView) findViewById(R.id.resultadosCalculadoraValueValorDoTitulo);
-            txtViewValorDoTitulo.setText(StringValorDoTitulo);
-
-            TextView txtViewValorDaParcela = (TextView) findViewById(R.id.resultadosCalculadoraValueValorParcela);
-            txtViewValorDaParcela.setText(StringValorDaParcela);
-
-            TextView txtViewSaldoAPagar = (TextView) findViewById(R.id.resultadosCalculadoraValueSaldoPagar);
-            txtViewSaldoAPagar.setText(StringSaldoAPagar);
-
-            TextView txtViewValorDaParcelaFinal = (TextView) findViewById(R.id.resultadosCalculadoraValueParcelaFinal);
-            txtViewValorDaParcelaFinal.setText(StringValorDaParcelaFinal);
+//            TextView txtViewUpgrade = (TextView) findViewById(R.id.resultadosCalculadoraValueUp);
+//            txtViewUpgrade.setText(StringUpgrade);
+//
+//            TextView txtViewValorDoTitulo = (TextView) findViewById(R.id.resultadosCalculadoraValueValorDoTitulo);
+//            txtViewValorDoTitulo.setText(StringValorDoTitulo);
+//
+//            TextView txtViewValorDaParcela = (TextView) findViewById(R.id.resultadosCalculadoraValueValorParcela);
+//            txtViewValorDaParcela.setText(StringValorDaParcela);
+//
+//            TextView txtViewSaldoAPagar = (TextView) findViewById(R.id.resultadosCalculadoraValueSaldoPagar);
+//            txtViewSaldoAPagar.setText(StringSaldoAPagar);
+//
+//            TextView txtViewValorDaParcelaFinal = (TextView) findViewById(R.id.resultadosCalculadoraValueParcelaFinal);
+//            txtViewValorDaParcelaFinal.setText(StringValorDaParcelaFinal);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -91,5 +101,9 @@ public class ResultadosCalculadora extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void novoCalculo() {
+        setContentView(R.layout.activity_calculadora);
     }
 }
