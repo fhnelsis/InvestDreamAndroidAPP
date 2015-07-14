4c4dcb46013af64b81650497c76e7e6add203763
Francisco Nelsis <fhnelsis@outlook.com>
2015-07-14 00:40:50 -0300

    20151307 - Correção de cálculo de contrato. Troca de EditText por Spinner no valor de entrada, e adição de Spinner de Classe.

--- app/src/main/java/br/com/investdream/investdreamandroidapp/Calculadora.java
+++ app/src/main/java/br/com/investdream/investdreamandroidapp/Calculadora.java
@@ -3,25 +3,17 @@ package br.com.investdream.investdreamandroidapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
-
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 
 public class Calculadora extends Activity {
 
-    private Spinner spinner;
+    private Spinner spinnerPercentualDeEntrada;
+    private Spinner spinnerClasse;
     private Button btCalcular;
 
     @Override
@@ -32,12 +24,7 @@ public class Calculadora extends Activity {
         final EditText maskedValorDoBem = (EditText) findViewById(R.id.fieldValorDoBem);
         maskedValorDoBem.addTextChangedListener(new MascaraMonetaria(maskedValorDoBem));
 
-//        final EditText maskedValorDaEntrada = (EditText) findViewById(R.id.spinnerPercentualDeEntrada);
-//        maskedValorDaEntrada.addTextChangedListener(new MascaraMonetaria(maskedValorDaEntrada));
-
-        addItemsOnSpinner();
-        addListenerOnButton();
-        addListenerOnSpinnerItemSelection();
+        addListenerBtCalcular();
     }
 
     @Override
@@ -62,108 +49,40 @@ public class Calculadora extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void addItemsOnSpinner() {
 
-        spinner = (Spinner) findViewById(R.id.spinnerPercentualDeEntrada);
-        List<String> list = new ArrayList<String>();
-        list.add("list 1");
-        list.add("list 2");
-        list.add("list 3");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
-    }
 
-    public void addListenerOnSpinnerItemSelection() {
-        spinner = (Spinner) findViewById(R.id.spinnerPercentualDeEntrada);
-        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-    }
-
-    // get the selected dropdown list value
-    public void addListenerOnButton() {
-
-        spinner = (Spinner) findViewById(R.id.spinnerPercentualDeEntrada);
-
+    public void addListenerBtCalcular() {
         btCalcular = (Button) findViewById(R.id.btCalcular);
 
         btCalcular.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View v) {
-
-                Toast.makeText(Calculadora.this,
-                        "OnClickListener : " +
-                                "\nSpinner 2 : " + String.valueOf(spinner.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                calcular(view);
             }
-
         });
     }
 
     public void calcular(View view) {
         Intent intent = new Intent(this, ResultadosCalculadora.class);
 
-
-
         EditText valordoBem = (EditText) findViewById(R.id.fieldValorDoBem);
-        EditText valordaEntrada = (EditText) findViewById(R.id.spinnerPercentualDeEntrada);
+
+        spinnerPercentualDeEntrada = (Spinner) findViewById(R.id.spinnerPercentualDeEntrada);
+        String StringSpinnerPercentualDeEntrada = spinnerPercentualDeEntrada.toString();
+
+        spinnerClasse = (Spinner) findViewById(R.id.spinnerClasse);
+        String StringSpinnerClasse = spinnerClasse.toString();
 
         Bundle bundle = new Bundle();
+
+        //TODO Resolver as strings das spinners que entram no bundle errado.
         bundle.putString("valorDoBem", valordoBem.getText().toString().replace("R$", "").replaceAll("\\.", "").replace(",", "."));
-        bundle.putString("valordaEntrada", valordaEntrada.getText().toString().replace("R$", "").replaceAll("\\.", "").replace(",", "."));
+        bundle.putString("StringSpinnerPercentualDeEntrada", StringSpinnerPercentualDeEntrada);
+        bundle.putString("StringSpinnerClasse", StringSpinnerClasse);
+        //bundle.putString("valordaEntrada", valordaEntrada.getText().toString().replace("R$", "").replaceAll("\\.", "").replace(",", "."));
         intent.putExtras(bundle);
         startActivity(intent);
     }
 
-    private class MascaraMonetaria implements TextWatcher {
-        final EditText campo;
 
-        public MascaraMonetaria(EditText campo) {
-            super();
-            this.campo = campo;
-        }
-
-        private boolean isUpdating = false;
-        // Pega a formatacao do sistema, se for brasil R$ se EUA US$
-        private NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int after) {
-            // Evita que o método seja executado varias vezes.
-            // Se tirar ele entre em loop
-            if (isUpdating) {
-                isUpdating = false;
-                return;
-            }
-            isUpdating = true;
-            String str = s.toString();
-            // Verifica se já existe a máscara no texto.
-            boolean hasMask = ((str.indexOf("R$") > -1 || str.indexOf("$") > -1) && (str.indexOf(".") > -1 || str.indexOf(",") > -1));
-            // Verificamos se existe máscara
-            if (hasMask) {
-                // Retiramos a máscara.
-                str = str.replaceAll("[R$]", "").replaceAll("[,]", "").replaceAll("[.]", "");
-            }
-            try {
-                // Transformamos o número que está escrito no EditText em
-                // monetário.
-                str = nf.format(Double.parseDouble(str) / 100);
-                campo.setText(str);
-                campo.setSelection(campo.getText().length());
-            } catch (NumberFormatException e) {
-                s = "";
-            }
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            // Não utilizado
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            // Não utilizado
-        }
-    }
 }
